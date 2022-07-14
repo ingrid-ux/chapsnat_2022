39822bff2719486c8323f3602e972d7bf731219d
Ingrid Barrera <ingridbbee@gmail.com>
2022-07-14 14:38:24 -0700

    finished lab 3c

--- App.js
+++ App.js
@@ -1,131 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState, useCallback, useEffect } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { GiftedChat, Video } from 'react-native-gifted-chat'
-import db from "./firebase";
+import React from "react";
+import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import ChatScreen from "./screens/ChatScreen";
+import HomeScreen from "./screens/HomeScreen";
 
+const Stack = createStackNavigator();
 
-
-export default function App() {
-
-  const [messages, setMessages] = useState([]); //call back hook
-  const [placeholder, setPlaceHolder] = useState("type a message")
-  //const [showUserAvatar, setUserAvatar] = useState([]); 
-  //const [alwaysShowSend, setalwaysShowSend] = useState([]); 
-
-    useEffect(() => {
-      db.collection("Chats")
-        .doc("myfirstchat")
-        .get()
-        .then((snapshot) => {
-          console.log(snapshot.id);
-          console.log(snapshot.data());
-        });
-    }, []);
-
-  // useEffect(() => {
-  //   setMessages([
-  //     {
-  //       _id: 1,
-  //       text: placeholder,
-  //       createdAt: new Date(),
-  //       user: {
-  //         _id: 1,
-  //         name: 'React Native',
-  //         avatar: 'https://placeimg.com/140/140/any',
-  //       },
-  //       image: 'https://placeimg.com/140/140/any',
-        
-  //     },
-
-
-  //     {
-  //       _id: 2,
-  //       text: 'hello world',
-  //       createdAt: new Date(),
-  //       user: {
-  //         _id: 2,
-  //         name: 'Reafdfsfct Native',
-  //         avatar: 'https://placeimg.com/140/140/any',
-  //       },
-  //       video: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
-  //     },
-      
-  //     {
-  //       _id: 6,
-  //       text: 'hello world',
-  //       createdAt: new Date(),
-  //       user: {
-  //         _id: 6,
-  //         avatar: 'https://i.ytimg.com/vi/Zr-qM5Vrd0g/maxresdefault.jpg',
-  //         text: 'hello!',
-       
-  //       },
-  //     },
-
-
-
-
-
-  //   ])
-  // }, [])
-
-  const onSend = useCallback((messages = []) => {
-    setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
-  }, [])
-
-
-
+function App() {
   return (
-      <GiftedChat
-      messages={messages}
-      onSend={messages => onSend(messages)}
-
-
-      user={{
-        _id: 6,
-        name: 'Ingrid',
-        avatar: 'https://i.ytimg.com/vi/Zr-qM5Vrd0g/maxresdefault.jpg',
-        text: 'hello!',
-
-      }}
-      showUserAvatar={true}
-      alwaysShowSend={true}
-      renderUsernameOnMessage={true}
-      />
-
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Chat" component={ChatScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
   },
 });
 
-
-
-//////example: 
-// {
-//   _id: 1,
-//   text: 'My message',
-//   createdAt: new Date(Date.UTC(2016, 5, 11, 17, 20, 0)),
-//   user: {
-//     _id: 2,
-//     name: 'React Native',
-//     avatar: 'https://facebook.github.io/react/img/logo_og.png',
-//   },
-//   image: 'https://facebook.github.io/react/img/logo_og.png',
-//   // You can also add a video prop:
-//   video: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
-//   // Mark the message as sent, using one tick
-//   sent: true,
-//   // Mark the message as received, using two tick
-//   received: true,
-//   // Mark the message as pending with a clock loader
-//   pending: true,
-//   // Any additional custom parameters are passed through
-// }
+export default App;
--- firebase.js
+++ firebase.js
@@ -1,9 +1,9 @@
-import firebase from "@firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import "@firebase/firestore";
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
-// Your web app's Firebase configuration, which you copy-pasted from Step 6
-var firebaseConfig = {
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    // your stuff
     apiKey: "AIzaSyDNsGpr63xLAhAbF__Il9kQw9I1AFQ0nv8",
     authDomain: "chapsnat-b2bc0.firebaseapp.com",
     projectId: "chapsnat-b2bc0",
@@ -11,9 +11,39 @@ var firebaseConfig = {
     messagingSenderId: "1013682065301",
     appId: "1:1013682065301:web:f465a3b37dfab58ce7709c",
     measurementId: "G-VNX4RF1R7N"
+
+        // apiKey: "AIzaSyC7CQwBSzjC_tlEiMd2Mc8Sh9Fb_Cwc1p8",
+        // authDomain: "chapsnat-3f4f7.firebaseapp.com",
+        // projectId: "chapsnat-3f4f7",
+        // storageBucket: "chapsnat-3f4f7.appspot.com",
+        // messagingSenderId: "239440555368",
+        // appId: "1:239440555368:web:d7d431a3733e778d273add",
+        // measurementId: "G-W4Y70B8JL2",
+    
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-let firestore = firebase.firestore();
-export default firestore;
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+export default db;
+
+
+// import firebase from "@firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// import "@firebase/firestore";
+
+// // Your web app's Firebase configuration, which you copy-pasted from Step 6
+// var firebaseConfig = {
+//     apiKey: "AIzaSyDNsGpr63xLAhAbF__Il9kQw9I1AFQ0nv8",
+//     authDomain: "chapsnat-b2bc0.firebaseapp.com",
+//     projectId: "chapsnat-b2bc0",
+//     storageBucket: "chapsnat-b2bc0.appspot.com",
+//     messagingSenderId: "1013682065301",
+//     appId: "1:1013682065301:web:f465a3b37dfab58ce7709c",
+//     measurementId: "G-VNX4RF1R7N"
+// };
+
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+// let firestore = firebase.firestore();
+// export default firestore;
